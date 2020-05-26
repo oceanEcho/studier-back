0d4cc434f4314ff5ce43acd436fca36fa990cb07
Ivan Zaytsev <sinde.fess@gmail.com>
2020-05-26 20:57:04 +0400

    add koa and koa-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const cors = require('@koa/cors');
 
 const app = new Koa();
+const router = new Router();
 
 app.use(cors());
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 // logger
 
@@ -24,7 +30,20 @@ app.use(async (ctx, next) => {
 
 // response
 
+app.use(async (ctx, next) => {
+  console.log(ctx.request)
+
+  const {request: {header: {origin}}} = ctx;
+ 
+  if (origin === 'http://localhost:3001') {
+    await next();
+  } else {
+    ctx.throw(401);
+  }
+});
+
 app.use(async (ctx) => {
+  console.log(ctx.request)
   ctx.body = {
     id: '571089fa-7d2b-4660-9740-4edcbf4dcdbb',
     name: 'economic.html',
