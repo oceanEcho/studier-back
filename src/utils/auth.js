74c63c326fefe8f843f471b5b97ac1878adc8c04
Ivan Zaytsev <sinde.fess@gmail.com>
2020-08-13 20:58:44 +0400

    revert last change

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,18 +2,16 @@ const jwt = require('jsonwebtoken');
 const process = require('process');
 
 const authenticateToken = (req, res, next) => {
-  if (process.env.SKIP_AUTH === '1') {
-    next();
-  }
-
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
+
   if (!token) {
     return res.status(401).send({
       status: 401,
       message: 'Вы не авторизованы',
     });
   }
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).send({
